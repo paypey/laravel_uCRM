d1e8b2cc4b9066a21bf00d1dc9a5506bb5001166
paypey <sincin.mi2@gmail.com>
2023-07-03 20:50:31 +0900

    Inertia-vue.js

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue';
+import Label from '@/Components/InputLabel.vue';
+
+</script>
+
+<template>
+<GuestLayout>
+  <Label value="件名">タイトル</Label>
+  <Input Value="初期値が入ります"></Input>
+</GuestLayout>
+
+</template>
